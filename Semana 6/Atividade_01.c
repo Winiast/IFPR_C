b525a9eabb7e5673c9927699d5fb784986cf6524
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2021-09-12 22:53:40 -0300

    Atividade_01 Feita

--- Semana 6/Atividade_01.c
+++ Semana 6/Atividade_01.c
@@ -2,5 +2,33 @@
 
 int main(){
 
+    int numero, Maior, Menor;
+
     
+  
+    printf("Numero ");
+    scanf("%i", &numero);
+
+    Maior = numero;
+    Menor = numero;
+        
+    while(numero >= 0){
+
+        if (numero > Maior){
+            Maior = numero;
+        }
+        if (numero < Menor){
+            Menor = numero;
+        }
+
+        printf("Numero ");
+        scanf("%i", &numero);
+        
+
+
+
+    }
+    printf("- Maior: %i", Maior);
+    printf("\n- Menor %i", Menor);
+
 }
