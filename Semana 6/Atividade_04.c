d8f1aafab9a384f1dedce6409463b8093a95b505
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2021-09-16 12:13:08 -0300

    Começando Semana 7

--- Semana 6/Atividade_04.c
+++ Semana 6/Atividade_04.c
@@ -7,14 +7,18 @@ int main(){
     // Pegando o numero
     printf("Digite um numero: ");
     scanf("%i", &Valor);
+
     //Setando valores das variaveis
     contador = 1;
     TestePrimos = 0;
     contadorPrimeza = 0;
+
     //Entrada do laco conferindo enquanto o contador for menor que o valor informado
     while(contador <= Valor){
+
         //Verificando se o valor da 0
         TestePrimos = Valor % contador;
+        
         // Colando no contador se for 0
         if(TestePrimos == 0){
             contadorPrimeza = contadorPrimeza + 1;
---
+++ Semana 7/Atividade_02.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    int largura, altura, cont_largura, cont_altura;
+    printf("Largura e altura: ");
+    scanf("%i %i", &largura, &altura);
+
+    cont_largura = 1;
+    cont_altura= 1;
+
+    while(cont_altura <= altura)
+    {   
+        printf("\n");
+        cont_largura = 1;
+
+        while(cont_largura <= largura){
+            printf("*");
+            cont_largura =  cont_largura + 1;
+        }
+
+        cont_altura = cont_altura + 1;
+    }
+    
+
+}
