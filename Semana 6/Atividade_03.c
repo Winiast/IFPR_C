b58ec15da7c3bd5282fd7efa1d34729f4ac828ab
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2021-09-15 21:41:24 -0300

    Começando a Resolver o 4, e entendi o 3

--- Semana 6/Atividade_03.c
+++ Semana 6/Atividade_03.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(){
-    char LetraInicio, LetraFim;
+    char LetraInicio, LetraFim, aumenta;
     char a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, y, z;
 
     printf("Digite dois caracteres: ");
-    scanf("%c %c", &LetraInicio, &LetraFim);
+    scanf("%c %c",&LetraInicio, &LetraFim);
+    
+    LetraInicio = toupper(LetraInicio);
+    LetraFim = toupper(LetraFim);
+    printf("%c %c", LetraInicio, LetraFim);
 
     a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10;
     
--- Semana 6/Atividade_04.c
+++ Semana 6/Atividade_04.c
@@ -2,21 +2,19 @@
 
 int main(){
     
-    int Valor, contador;
+    int Valor, contador, TestePrimos;
+
     printf("Digite um numero: ");
     scanf("%i", &Valor);
 
-    while(Valor / 2 != 0){
+    contador = 0;
 
-        if(contador > 2){
-            printf("[Nao Primo]");
-        }
-        else{
-            printf("[Primo]");
-        }
-
+    while(contador != Valor){
+        TestePrimos = Valor % contador;
         contador = contador + 1;
-
+        printf("%i", contador);
+        printf("%i", TestePrimos);
     }
-    printf("%i", contador);
+
+    
 }
