42aa91207783c2bfe5872928f6810df32218260e
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2021-09-25 22:49:46 -0300

    Fé na Semana 8

---
+++ Semana 8/Atividade_02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int valor, ini, fim, box, box01;
+
+    printf("Digite um valor inteiro: ");
+    scanf("%i ", &valor);
+
+    box = valor - 1;
+
+    for(ini = 0; valor; valor = valor -1){
+
+        pritf("%i x ", valor);
+
+    }
+
+
+    }
+}
