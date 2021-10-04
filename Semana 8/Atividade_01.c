81668d7f2e43dc0003ce5b318f21ef85cf0c220b
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2021-10-04 10:53:20 -0300

    2 Hrs e termino semana 8

--- Semana 8/Atividade_01.c
+++ Semana 8/Atividade_01.c
@@ -3,4 +3,30 @@
 
 int main() {
 
+    int fatorial =0, cont, valor = 0, flag=1,cont_02 = 0, soma =0;
+     
+    printf("Digite um valor inteiro: ");
+    scanf("%i", &valor);
+
     
+
+    printf("\n - Fatorial(%i) = ", valor);
+
+    fatorial = valor;
+
+     for(cont=0; valor > cont; valor--){
+
+         
+        fatorial = valor * cont_02;
+        soma = soma + fatorial;
+
+        if(flag == 1){
+            printf(" %i", valor);
+            flag = 0;
+        }
+        else{
+        printf(" x %i", valor);
+        }
+     }
+     printf(" = %i", soma);
+}
---
+++ Semana 8/Atividade_04.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+    int v1, v2 ,v3 ,v4 ,v5 ,v6 ,v7;
+    
+    printf("Digite 7 números: ");
+    scanf("%i %i %i %i %i %i %i", &v1, &v2, &v3, &v4, &v5, v6, v7);
+
+    if(v1 > 0 && v1 <= 1000, v2 > 0 && v2 <= 1000,v3 > 0 && v3 <= 1000,
+    v4 > 0 && v4 <= 1000,v5 > 0 && v5 <= 1000,
+    v6 > 0 && v6 <= 1000,v7 > 0 && v7 <= 1000){
+
+        while( v1 > v2){
+
+        }
+
+    }
+
+    else{
+        printf("Número digitados ultrapassam o valor de 1000 ou são menores que 1");
+    }
+}
