b5dfc2523ea05a818ecbfddab5d95973fdaa06e5
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2021-10-12 23:15:40 -0300

    Tentando ainda

--- Semana 8/Atividade_04.c
+++ Semana 8/Atividade_04.c
@@ -2,19 +2,23 @@
 
 int main(){
 
-    int valor[8], i = 1, flag = 1, menor = 0;
+    int valor[8],count=0, i = 1, flag = 1, maior = 0;
     
     printf("Digite 7 numeros: ");
     scanf("%i %i %i %i %i %i %i", &valor[1],&valor[2],&valor[3], &valor[4], &valor[5], &valor[6], &valor[7]);
 
     printf("- Ordem crescente: ");
     while(i < 8){
+
+
+            if(valor[i] > maior){
+                maior = valor[i];
+            }
         
-        if(valor[i] < menor){
-            menor = valor[i];
-        }
         printf("%i ",valor[i]);
         i ++;
     }
+
+    printf("\n %i", maior);
 
 }
