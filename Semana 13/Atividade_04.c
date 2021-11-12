95667e02d11cafa8e261678efd65b5ff5ebd3aee
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2021-11-12 01:21:01 -0300

    Semana 13 completa, falta semana 12 só

--- Semana 13/Atividade_04.c
+++ Semana 13/Atividade_04.c
@@ -1,22 +1,59 @@
 #include <stdio.h>
-int main ()
-{
-	int num;
-	printf ("Digite um numero: ");
-	scanf ("%d",&num);
-	switch (num)
-        {
-        	case 9:
-                	printf ("\n\nO numero e igual a 9.\n");
-        	break;
-        	case 10:
-                	printf ("\n\nO numero e igual a 10.\n");
-        	break;
-        	case 11:
-                	printf ("\n\nO numero e igual a 11.\n");
-        	break;
-        	default:
-                	printf ("\n\nO numero nao e nem 9 nem 10 nem 11.\n");
+#include <stdlib.h>
+#include <time.h>
+
+#define ANSI_COLOR_RED      "\x1b[31m" //cores em ANSI utilizadas 
+#define ANSI_COLOR_GRAY     "\e[0;37m"
+#define TAM 5
+
+int main(){
+
+    srand(time(NULL));
+
+    int matriz[5][5], lin, colun, linha_troca, valor_troca;
+
+    for(lin=0; lin <TAM; lin++){
+
+        for(colun=0; colun<TAM; colun++){
+
+            matriz[lin][colun] = 10 + (rand() % 41);
+
         }
-	return(0);
+    }
+
+    printf("\nMatriz Aleatoria: \n");
+
+    for(lin=0; lin <TAM; lin++){
+
+        for(colun=0;colun<TAM;colun++){
+            printf("%i  ", matriz[lin][colun]);
+        }
+
+        printf("\n");
+	}
+	printf("\n");
+
+	printf("- Escolha uma linha: ");
+	scanf("%i", &linha_troca);
+
+	printf("- Escolha um valor: ");
+	scanf("%i", &valor_troca);
+
+	printf("\nMatriz Modificada:");
+
+	for(lin=0; lin < TAM; lin++){
+			printf("\n");
+			if(lin != linha_troca){
+				for(colun=0; colun<TAM; colun++){
+					printf("%i  ", valor_troca);
+				}
+			}
+			else{
+				for(colun=0; colun < TAM; colun++){
+				printf(ANSI_COLOR_RED "%i  "ANSI_COLOR_GRAY  , matriz[lin][colun]);
+				}
+			}	
+	}
+
+	
 }
