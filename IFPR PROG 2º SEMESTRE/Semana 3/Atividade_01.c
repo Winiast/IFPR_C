ab967429b488b3a3a4986b7758cdba37895247b1
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2022-02-27 20:30:23 -0300

    Semana 3 completa

--- IFPR PROG 2º SEMESTRE/Semana 3/Atividade_01.c
+++ IFPR PROG 2º SEMESTRE/Semana 3/Atividade_01.c
@@ -1,9 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
 
-    char *ponteiro_01[20], *ponteiro_02[20];
+    char *ponteiro_01, *ponteiro_02;
+    char palavra01[50], palavra02[50];
+    int tamanho01, tamanho02;
+    int posicao = 0;
+    char diferente01[1], diferente02[1];
 
+    fflush(stdin);
+
+    printf("Primeira Palavra: ");
+    gets(palavra01);
+    fflush(stdin);
+    printf("Segunda Palavra: ");
+    gets(palavra02);
+
+    tamanho01 = strlen(palavra01);
+    tamanho02 = strlen(palavra02);
+
+    ponteiro_01 = malloc(tamanho01*sizeof(char));
+    ponteiro_02 = malloc(tamanho02*sizeof(char));
+
+    ponteiro_01 = palavra01;
+    ponteiro_02 = palavra02;
+    int i;
+
+    int maiorpalavra;
+    
+    if(tamanho01 < tamanho02){
+        maiorpalavra = tamanho02;
+    }
+    else{
+        maiorpalavra = tamanho01;
+    }
+
+    for(i = 0; i < maiorpalavra; i++){
+        if(ponteiro_01[i] != ponteiro_02[i]){
+            posicao = i;
+            break;
+        }
+    }
+
+    printf("- Diferentes, posicao: %i: '%c' != '%c'", posicao, ponteiro_01[posicao], ponteiro_02[posicao]);
+
+    free(ponteiro_01);
+    free(ponteiro_02);
     
 }
